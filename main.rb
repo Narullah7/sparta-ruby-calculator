44469aa6c1d2b786ffc17a9f78e7becffb2ccd28
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-14 20:42:53 +0000

    functions loops itself after a calculation is done

--- main.rb
+++ main.rb
@@ -1,3 +1,22 @@
-puts "----Calculator----"
-puts "Please select from the following. type 'B' for basic, 'A' for advanced and 'M for BMI'? "
-type_of_calc = gets.chomp
+# function for basic Calculator
+def basic_calc
+  puts "Hey there"
+  display_calculator
+end
+# function for advanced Calculator
+# function for BMI Calculator
+
+
+
+
+#Create initial message and ask for user prompt
+def display_calculator
+  puts "----Calculator----"
+  puts "Please select from the following. type 'B' for basic, 'A' for advanced and 'M for BMI'? "
+  type_of_calc = gets.chomp()
+  if (type_of_calc == "B")
+    basic_calc
+  end
+
+end
+display_calculator
