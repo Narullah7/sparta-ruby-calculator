2967fb5fc13c2d1b5c9d02904eb6f750a2ca93b6
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-14 22:19:39 +0000

    Advanced calculator operator works

--- main.rb
+++ main.rb
@@ -11,6 +11,8 @@ def basic_calc
     num2 = gets.chomp()
     sum = num1.to_f + num2.to_f
     puts "Your answer is #{sum}"
+    clear()
+    display_calculator
 
   when "-"
     puts "Choose your first number? "
@@ -19,6 +21,7 @@ def basic_calc
     num2 = gets.chomp()
     sum = num1.to_f - num2.to_f
     puts "Your answer is #{sum}"
+    display_calculator
 
   when "*"
     puts "Choose your first number? "
@@ -27,6 +30,7 @@ def basic_calc
     num2 = gets.chomp()
     sum = num1.to_f * num2.to_f
     puts "Your answer is #{sum}"
+    display_calculator
 
   when "/"
     puts "Choose your first number? "
@@ -35,12 +39,39 @@ def basic_calc
     num2 = gets.chomp()
     sum = num1.to_f / num2.to_f
     puts "Your answer is #{sum}"
+    display_calculator
   end
-
-
-  # display_calculator
 end
+
 # function for advanced Calculator
+def advanced_calc
+  puts "You chose advanced calculator, which operator would you like to use? type 'P' for power or 'S' for square-root? "
+  operator = gets.chomp()
+
+  case operator
+    when "P"
+      puts "Choose your base number? "
+      num1 = gets.chomp()
+      puts "Choose your exponential number? "
+      num2 = gets.chomp()
+      sum = (num1.to_f) ** num2.to_f
+      puts "Your answer is #{sum}"
+      display_calculator
+
+    when "S"
+      puts "Choose your number? "
+      x = gets.chomp().to_f
+      if (x < 0)
+        puts "Error, you cannot divide by a negative number"
+        display_calculator
+      elsif (x >= 0)
+        sum = Math.sqrt(x)
+        puts "Your answer is #{sum}"
+        display_calculator
+      end
+  end
+end
+
 # function for BMI Calculator
 
 
@@ -53,6 +84,8 @@ def display_calculator
   type_of_calc = gets.chomp()
   if (type_of_calc == "B")
     basic_calc
+  elsif (type_of_calc == "A")
+    advanced_calc
   end
 
 end
